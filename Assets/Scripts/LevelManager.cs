6474ae8f3a1cef8c95b66b4f684ed00fd4b850a0
Tejas Khorana <tejaskhorana@gatech.edu>
2016-11-08 03:20:46 -0500

    Number Wizard with User Interface

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void callLevel (string name) {
+		Application.LoadLevel(name);
+	}
+
+	public void quitGame (string name) {
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	int maxNumGuesses = 5;
+	int currNumGuesses = 0;
+
+	public Text currGuess;
+
+	void Start() {
+		StartGame();
+	}
+	
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		guess = NextGuess();
+		currGuess.text = guess.ToString();
+		currNumGuesses+=1;
+	}
+
+
+	public void GuessHigher()
+	{
+		if (currNumGuesses == maxNumGuesses) {
+			WinGame();
+		}
+		min = guess;
+		guess = NextGuess();
+		currGuess.text = guess.ToString();
+		currNumGuesses+=1;
+	}
+
+	public void GuessLower()
+	{
+		if (currNumGuesses == maxNumGuesses) {
+			WinGame();
+		}
+		max = guess;
+		guess = NextGuess();
+		currGuess.text = guess.ToString();
+		currNumGuesses+=1;
+	}
+
+	int NextGuess() {
+		guess = Random.Range(min, max+1);
+		return guess;
+	}
+
+	void WinGame() {
+		Application.LoadLevel("Win");
+	}
+}
